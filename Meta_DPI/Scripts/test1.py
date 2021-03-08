cd3bddc79e5d79ad77974702ea158839ec6b1498
Eved1018 <edelsteinevan@gmail.com>
2021-03-08 14:54:56 -0500

    commit

---
+++ Meta_DPI/Scripts/test1.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from pathlib import Path
+# cols = ["x","y","z","a","b"]
+# df = pd.read_csv("/Users/evanedelstein/Desktop/PDBtest.csv")
+# print(df.columns.tolist())
+# if df.columns.tolist() != cols:
+#     print("changing cols")
+#     df.columns = cols
+#     print(df.columns.tolist())
+# else:
+#     print("all good")
+
+# proteinids = []
+#     for resprot in proteinname: 
+#         proteinid = resprot.split("_")[1]
+#         if proteinid not in proteinids:
+#              proteinids.append(proteinid)
+
+# df = pd.read_csv("/Users/evanedelstein/Desktop/PDBtest.csv")
+# for i in df.columns.tolist():
+#     df = "apple"
+
+# li = ["L","b","c"]
+# K = "k"
+# tup = [(i,K) for i in li]
+# print(tup)
+# path = Path(__file__).resolve().parent.parent.parent 
+# path2 = Path(__file__).parents[2]
+# if path == path2:
+#     print("true")
+# else:
+# #     print("fasle")
+# x = 5 
+# y = 7
+# if x == 5 and y ==7:
+#     print("hello")
+# df = pd.DataFrame(columns=["A","B"])
+# df = df.rename(columns={"A":"C"})
+# print(df.head())
