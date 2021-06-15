500a1fcb6c51cc02cab87ee6c98a6a4cf405805b
eved1018 <edelsteinevan@gmail.com>
2021-06-15 10:29:06 -0400

    commit

--- Meta_DPI/Scripts/F_score_MCC_paralel.py
+++ Meta_DPI/Scripts/F_score_MCC_paralel.py
@@ -5,27 +5,32 @@ import concurrent.futures
 import multiprocessing
 from numpy import sqrt
 from pathlib import Path
+
+
 # fscore and mcc  per protein(saved as csv) and per predictor (printed)
 
 
-def Main():
-    path = Path(__file__).resolve().parent.parent.parent
+def Main(predictors,df,result_path,code):
+    results_folder = f"{result_path}/Meta_DPI_results{code}/Fscore_MCC/"
+    path = Path(__file__).parents[2]
 #   predictors = ["vorffip"]
 #   check that predictor is in columns
-    predictors = ['vorffip']
+    # predictors = ['vorffip']
     # path ="/Users/evanedelstein/Desktop/Research_Evan/Raji_Summer2019_atom/Data_Files/Meta_DPI/META_DPI_RESULTS3/Meta_DPi_result.csv"
-    data_path = f"{path}/Meta_DPI/Data/Test_data/vorffip_reformatted.csv"
+    # data_path = f"{path}/Meta_DPI/Data/Test_data/vorffip_columns.txt" #<- TODO change to metta ppisp 
     # path = "/Users/evanedelstein/Desktop/1p_test.csv"
-    result_path = f"{path}/Meta_DPI/Results/Fscore_MCC/"
+    # result_path = f"{path}/Meta_DPI/Results/Fscore_MCC/"
+    # result_path = f"{path}/Meta_DPI/Results/Fscore_MCC/Zerotest/" 
     cutoff_path = f"{path}/Meta_DPI/Data/Test_data/All_protein_cutoffs.csv"
-    # cutoff = (6.1^-residues)(constant) 
-    # F_score(predictors,path,cutoff_path)
+    # cutoff = (6.1^-residues)(constant) # <- TODO make dynamic cutoff in script 
     cutoff_csv = pd.read_csv(cutoff_path)
-    df = pd.read_csv(data_path)
-    df.set_index('residue', inplace= True )
+    
+    cut_off_protein = cutoff_csv["Protein"].tolist()
+    # df = pd.read_csv(data_path)
+    # df.set_index('residue', inplace= True )
     df["protein"] = [x.split('_')[1] for x in df.index]
     proteins = df["protein"].unique()
-    # print(proteins)
+    proteins = [i for i in proteins if i in cut_off_protein]
     cols = ["protein"] + predictors 
     f_score_per_protein = pd.DataFrame(columns = cols)
     f_score_per_protein["protein"] = proteins
@@ -34,8 +39,7 @@ def Main():
     mcc_score_per_protein["protein"] = proteins
     mcc_score_per_protein.set_index('protein', inplace=True)
     np.seterr(all='raise')
-    for ∆ in predictors:
-            # print(predictor)
+    for predictor in predictors:
             TP_sum = 0
             FP_sum =0
             Ns_sum = 0
@@ -49,7 +53,7 @@ def Main():
                 params_list.append(params)
             with concurrent.futures.ProcessPoolExecutor() as executor:
                 params_list = params_list
-                results = executor.map( Run, params_list)
+                results = executor.map(Run, params_list)
                 for i in results:
                     (TP, TN, FP, FN, threshhold, N,predictor, protein) = i
                     TP_sum += TP
@@ -58,26 +62,19 @@ def Main():
                     FN_sum += FN
                     TN_sum += TN
                     threshold_sum += threshhold
-                    recall_p = TP/N
-                    precision_p = TP/threshhold
-                    # print(protein)
-                    # print("TP:",TP)
-                    # print("FP:",FP)
-                    # print("N:",N)
-                    # print("FN:",FN)
-                    # print("TN",TN)
-                    # print("1:",(TP * TN))
-                    # print("2",(FP * FN))
-                    MCC_num = (TP * TN) - (FP * FN)
-                    mcc_denom = sqrt((TP + FP) * (TP + FN)  * (TN + FP) * (TN + FN))
-                    mcc = MCC_num / mcc_denom
+                    recall_f_score = TP/N
+                    precision_f_score = TP/threshhold
+                    
                     if TP != 0:
-                        f_score = (2 * recall_p *precision_p)/(recall_p + precision_p)
+                        f_score = (2 * recall_f_score *precision_f_score)/(recall_f_score + precision_f_score)
+                        MCC_num = (TP * TN) - (FP * FN)
+                        mcc_denom = sqrt((TP + FP) * (TP + FN)  * (TN + FP) * (TN + FN))
+                        mcc = MCC_num / mcc_denom
                         
                     else:
                         f_score = 0
+                        mcc = 0 # <- TODO do once with 0 and another wiht undefinded 
                         
-                    # print("mcc", mcc)
                     f_score_per_protein.loc[protein,predictor] = f_score
                     mcc_score_per_protein.loc[protein,predictor] = mcc 
 
@@ -92,12 +89,11 @@ def Main():
             mcc = MCC_num / mcc_denom
             print("{} Global MCC: {}".format(predictor,np.round(mcc,3)) ) 
 
-            f_score_per_protein.to_csv("{}fscore_per_protein.csv".format(result_path))
-            mcc_score_per_protein.to_csv("{}mcc_per_protein.csv".format(result_path))
+            f_score_per_protein.to_csv(f"{results_folder}fscore_per_protein.csv")
+            mcc_score_per_protein.to_csv(f"{results_folder}mcc_per_protein.csv")
 
 def Run(params): 
     (predictor,protein,cutoff_csv,frame) = params
-    # print(predictor, protein)
     total_res = len(frame.index)
     annotated_frame = frame[frame['annotated'] == 1]
     annotated_res_prot = annotated_frame.index.tolist()
@@ -110,7 +106,7 @@ def Run(params):
     predicted_res = thresholdframe.index.values.tolist()
     predicted_res = [str(i) for i in predicted_res]
     pred_res = [i.split("_")[0] for i in predicted_res]
-    Truepos = [i for i in annotated_res if i in pred_res]
+    Truepos = [i for i in annotated_res if str(i) in pred_res]
     pred = len(pred_res)
     TP = len(Truepos)
     FP = pred - TP
@@ -118,9 +114,10 @@ def Run(params):
     FN = N - TP
     TN = neg - FN
     return TP, TN, FP, FN, threshhold, N ,predictor, protein
-    
+
+
            
 
 
-if __name__ == '__main__':
-    Main()
+# if __name__ == '__main__':
+#     Main()
